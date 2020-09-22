0374a4feaa1f0532122062d563e3e20e073f3b17
BenGlenn <68648238+BenGlenn@users.noreply.github.com>
2020-09-22 16:10:54 -0400

    Migration Trasaction Added

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        [Required]
+        [ForeignKey("Product")]
+        public int ProductID { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        [Required]
+        public int ItemCount { get; set; }
+
+        [Required]
+        public DateTime DateOfTransaction { get; set; }
+    }
+}
